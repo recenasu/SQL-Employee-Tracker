8f6a342c527b4ea5abfcb869fb999768288747ea
Domitius <133902698+recenasu@users.noreply.github.com>
2023-09-09 22:08:57 -0700

    Add some of the inquirer prompts, added console.table module

---
+++ server.js
@@ -0,0 +1,105 @@
+// Define variables and functions to import required modules.
+const fs = require('fs');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+require('dotenv').config();
+const consoleTable = require('console.table');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+// This function runs one of the canned queries on the database and prints the result to the console. The name of the .sql file containing the query is the argument.
+function runQuery(queryFile) {
+
+    // Connect to database
+    const db = mysql.createConnection(
+        {
+            host: 'localhost',
+            user: process.env.DB_USER,
+            password: process.env.DB_USER_PASSWORD,
+            database: process.env.DB_NAME
+        },
+    );
+    // Assign query .sql file to variable
+    let sql = fs.readFileSync(`./db/${queryFile}`, 'utf-8');
+
+    // Run query, display results in the console:
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('\n')
+        console.table(result);
+        init();
+    });
+}
+
+// This function executes the program.
+function init() {
+    console.log('**EMPLOYEE TRACKER**')
+
+    // Inquirer prompts:
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'initialSelection',
+                choices: [
+                    'View All Departments',
+                    'View All Roles',
+                    'View All Employees',
+                    'Add a Department',
+                    'Add a Role',
+                    'Add an Employee',
+                    'Update an Employee Role'
+                ]
+            }
+        ])
+        .then((response) => {
+
+            // Execute function based on selection.
+            switch (response.initialSelection) {
+                case 'View All Departments':
+                    runQuery('departments_query.sql');
+                    break;
+                case 'View All Roles':
+                    runQuery('roles_query.sql');
+                    break;
+                case 'View All Employees':
+                    runQuery('employees_query.sql');
+                    break;
+                case 'Add a Department':
+                    console.log('good choice!');
+                    // addDepartment();
+                    break;
+                case 'Add a Role':
+                    console.log('good choice!');
+                    // addRole();
+                    break;
+                case 'Add an Employee':
+                    console.log('good choice!');
+                    // addEmployee();
+                    break;
+                case 'Update an Employee Role':
+                    console.log('good choice!');
+                    // updateEmployeeRole();
+                    break;
+                default:
+                    console.log(`no selection`);
+            };
+        });
+        
+};
+
+init();
+
+
+
